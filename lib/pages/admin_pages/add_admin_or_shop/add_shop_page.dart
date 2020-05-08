c6e83a41d471bc467eaca510c39468a813431d2b
sasukeamjed <sasukeamjed@gmail.com>
2020-05-08 22:34:42 +0400

    adding image picker

--- lib/pages/admin_pages/add_admin_or_shop/add_shop_page.dart
+++ lib/pages/admin_pages/add_admin_or_shop/add_shop_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AddShopPage extends StatelessWidget {
+  double width = 200;
+  double height = 200;
+
   @override
   Widget build(BuildContext context) {
     print('Add Shop Page is Build');
@@ -11,26 +14,53 @@ class AddShopPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.red
+              width: width,
+              height: height,
+              child: Stack(
+                alignment: Alignment.bottomRight,
+                children: <Widget>[
+                  Container(
+                    width: width,
+                    height: height,
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      shape: BoxShape.circle
+                    ),
+                    child: Center(
+                      child: Image.asset('assets/shop.png'),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: -10,
+                    right: -10,
+                    child: Align(
+                      alignment: Alignment.bottomRight,
+                      child: Padding(
+                        padding: EdgeInsets.only(right:0.0, bottom: 0),
+                        child: IconButton(
+                          iconSize: width / 3,
+                          icon: Icon(Icons.add_a_photo),
+                          onPressed: (){},
+                        ),
+                      ),
+                    ),
+                  )
+                ],
               ),
             ),
             TextField(
               decoration: InputDecoration(
-                fillColor: Colors.white,
-                hintText: 'Shop Name'
-              ),
+                  fillColor: Colors.white, hintText: 'Shop Name'),
             ),
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 10,
+            ),
             TextField(
-              decoration: InputDecoration(
-                  hintText: 'Shop Owner Name'
-              ),
+              decoration: InputDecoration(hintText: 'Shop Owner Name'),
             ),
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 10,
+            ),
           ],
         ),
       ),
